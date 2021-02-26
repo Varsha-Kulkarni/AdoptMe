64f5208e8736e7aa2da396ab69861f97dcf83c39
VarshaSameer <km.varsha@gmail.com>
2021-02-26 08:41:38 +0530

    feat: add main and detail destinations

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,12 +18,28 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+import com.example.androiddevchallenge.ui.screens.PetDetail
+import com.example.androiddevchallenge.ui.screens.PetList
+import com.example.androiddevchallenge.ui.screens.defaultPets
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.utils.Actions
+import com.example.androiddevchallenge.utils.Destinations.Detail
+import com.example.androiddevchallenge.utils.Destinations.DetailArgs.DetailArg
+import com.example.androiddevchallenge.utils.Destinations.Main
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,12 +55,31 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
+
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+//        Text(text = "Ready... Set... GO!")
+
+        val navController = rememberNavController()
+        val actions = remember(navController) { Actions(navController) }
+        NavHost(
+            navController = navController,
+            startDestination = "main"
+        ) {
+            composable(Main) {
+                PetList(actions.openDetails, defaultPets)
+            }
+            composable("$Detail/{$DetailArg}", arguments = listOf(navArgument("petId") { type =
+                NavType.IntType
+            })) {
+                it.arguments?.getInt("petId")?.let { petId ->
+                    PetDetail(petId)
+                }
+            }
+        }
     }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
+//@Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
     MyTheme {
@@ -52,7 +87,7 @@ fun LightPreview() {
     }
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+//@Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
