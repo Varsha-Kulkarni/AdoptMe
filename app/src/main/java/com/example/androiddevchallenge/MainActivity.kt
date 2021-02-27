d55bb7271ca117ec19082de4387d1a3c566d7952
VarshaSameer <km.varsha@gmail.com>
2021-02-28 00:35:21 +0530

    update: entry point for the app to begin

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,81 +16,56 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.animation.Crossfade
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.navArgument
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.saveable.rememberSaveable
+import com.example.androiddevchallenge.models.petCollections
 import com.example.androiddevchallenge.ui.screens.PetDetail
 import com.example.androiddevchallenge.ui.screens.PetList
-import com.example.androiddevchallenge.ui.screens.defaultPets
 import com.example.androiddevchallenge.ui.theme.MyTheme
-import com.example.androiddevchallenge.utils.Actions
-import com.example.androiddevchallenge.utils.Destinations.Detail
-import com.example.androiddevchallenge.utils.Destinations.DetailArgs.DetailArg
-import com.example.androiddevchallenge.utils.Destinations.Main
+import com.example.androiddevchallenge.utils.*
+import dev.chrisbanes.accompanist.insets.ProvideWindowInsets
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyTheme {
-                MyApp()
-            }
-        }
-    }
-}
-
-// Start building your app here!
-@Composable
-fun MyApp() {
-
-    Surface(color = MaterialTheme.colors.background) {
-//        Text(text = "Ready... Set... GO!")
-
-        val navController = rememberNavController()
-        val actions = remember(navController) { Actions(navController) }
-        NavHost(
-            navController = navController,
-            startDestination = "main"
-        ) {
-            composable(Main) {
-                PetList(actions.openDetails, defaultPets)
-            }
-            composable("$Detail/{$DetailArg}", arguments = listOf(navArgument("petId") { type =
-                NavType.IntType
-            })) {
-                it.arguments?.getInt("petId")?.let { petId ->
-                    PetDetail(petId)
+            val systemUiController = remember { SystemUiController(window) }
+            CompositionLocalProvider(LocalSysUiController provides systemUiController) {
+                MyTheme {
+                    MyApp(backDispatcher = onBackPressedDispatcher)
                 }
             }
         }
     }
 }
 
-//@Preview("Light Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
-    }
-}
+// Start building your app here!
 
-//@Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+fun MyApp(backDispatcher: OnBackPressedDispatcher) {
+    val navigator: Navigator<Destination> = rememberSaveable(
+        saver = Navigator.saver(backDispatcher)
+    ) {
+        Navigator(Destination.Home, backDispatcher)
+    }
+    val actions = remember(navigator) { Actions(navigator) }
+    ProvideWindowInsets {
+        MyTheme {
+            Crossfade(navigator.current) { destination ->
+                when (destination) {
+                    Destination.Home -> PetList(actions.selectPet, petCollections)
+                    is Destination.Detail -> PetDetail(
+                        petId = destination.petId,
+                        upPress = actions.upPress
+                    )
+                }
+            }
+        }
     }
 }
