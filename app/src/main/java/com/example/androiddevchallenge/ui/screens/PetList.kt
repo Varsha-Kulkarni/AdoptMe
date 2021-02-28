a0a0dd27f243e11813cb0eab187a941bce27323e
VarshaSameer <km.varsha@gmail.com>
2021-02-28 09:52:03 +0530

    feat: display Pets in Grid of 2 columns, background box colors for better appearance

--- app/src/main/java/com/example/androiddevchallenge/ui/screens/PetList.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screens/PetList.kt
@@ -1,74 +1,212 @@
 package com.example.androiddevchallenge.ui.screens
 
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.Image
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.FilterList
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.R
+import com.example.androiddevchallenge.models.Filter
 import com.example.androiddevchallenge.models.Pet
+import com.example.androiddevchallenge.models.PetCollection
+import com.example.androiddevchallenge.models.PetRepo
+import com.example.androiddevchallenge.ui.components.PetImage
+import com.example.androiddevchallenge.ui.components.VerticalGrid
+import com.example.androiddevchallenge.ui.components.getBackgroundColorForElevation
+import dev.chrisbanes.accompanist.insets.statusBarsHeight
 
-
-val defaultPets = listOf(
-    Pet(0, "Sandy", "The Afador is a mixed breed dog–a cross between the Afghan Hound and Labrador Retriever dog breeds. Loyal, energetic, and affectionate, Afadors make excellent family pets, although they do better with older children.",R.drawable.afador,"Contact: Animals Matter To Me NGO, Mumbai","Afador","dog"),
-    Pet(1, "Mat", "The Beaglier is a mixed breed dog — a cross between the Beagle and Cavalier King Charles Spaniel dog breeds. Compact, energetic, and loyal, these adorable pups make great family additions for active singles or families with older children in large homes or small apartments.",R.drawable.beaglier,"Contact: PAWS, New Delhi","Beaglier", "dog"),
-    Pet(2, "Brownie", "The Bulldog was originally used to drive cattle to market and to compete in a bloody sport called bullbaiting. Today, they’re gentle companions who love kids.",R.drawable.bulldog,"Contact: Pet Lovers Society, Bangalore","Bulldog", "dog"),
-    Pet(3, "Chetak", "Loving, devoted, and energetic, Goldador mixed breed dogs are prized for their good-natured trainability. As a cross between the Golden Retriever and Labrador Retriever, this mix inherited some of the best traits from both purebred parents.",R.drawable.goldador,"Contact: Dog Temple, Goa","Goldador", "dog"),
-    Pet(4, "Pinky", "The Munchkin cat has no problem getting around the same as its longer-limbed feline friends — it just might take them a few extra steps along the way. These cute, curious cats are known for snatching shiny objects, so don’t be surprised if these “magpies” borrow your favorite piece of jewelry:)",R.drawable.munchkin,"Contact: Contact: Pet Lovers Society, Bangalore","Munchkin", "cat"),
-    Pet(5, "Purr", "“Himmies,” as they are sometimes called, are wonderful indoor cat companions. They are gentle, calm, and sweet-tempered, and possess a playful side as well.",R.drawable.himalayan,"Contact: Charlie's Animal Rescue Centre (CARE), Ahmedabad","Himalayan", "cat"),
-)
 
 @Composable
-fun PetList(openDetails: (Int) -> Unit, pets: List<Pet>) {
-    Scaffold(
-        topBar = {
-            TopAppBar(title = {
-                Text("Adopt Me!")
-            })
-        }) {
-        LazyColumn {
-            items(pets) {
-                PetCard(it, Modifier.padding(16.dp), openDetails = { openDetails(it.petId) })
+fun FilterBar(filters: List<Filter>) {
+    LazyRow(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(start = 8.dp, end = 8.dp),
+        modifier = Modifier.heightIn(min = 56.dp)
+    ) {
+        item {
+            IconButton(onClick = { /* todo */ }) {
+                Icon(
+                    imageVector = Icons.Rounded.FilterList,
+                    tint = MaterialTheme.colors.primary,
+                    contentDescription = stringResource(R.string.label_filters),
+                    modifier = Modifier.background(
+                        color = Color.White,
+                        shape = CircleShape
+                    )
+
+                )
             }
+        }
+        items(filters) { filter ->
+            FilterChip(filter)
         }
     }
 }
 
 @Composable
-fun PetCard(pet: Pet, modifier: Modifier, openDetails: () -> Unit = {}) {
+fun FilterChip(
+    filter: Filter,
+    modifier: Modifier = Modifier,
+    shape: Shape = MaterialTheme.shapes.small
+) {
+    val (selected, setSelected) = filter.enabled
+    val backgroundColor by animateColorAsState(
+        if (selected) MaterialTheme.colors.primary else Color.White
+    )
+    val textColor by animateColorAsState(
+        if (selected) Color.LightGray else MaterialTheme.colors.secondary
+    )
+    Surface(
+        modifier = modifier
+            .height(30.dp)
+            ,
+        color = backgroundColor,
+        contentColor = textColor,
+        shape = shape,
+        elevation = 2.dp
+    ) {
+        Box(
+            modifier = Modifier.toggleable(
+                value = selected,
+                onValueChange = setSelected
+            )
+        ) {
+            Text(
+                text = filter.name,
+                style = MaterialTheme.typography.caption,
+                maxLines = 1,
+                modifier = Modifier.padding(
+                    horizontal = 20.dp,
+                    vertical = 6.dp
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun PetList(selectPet: (Int) -> Unit, petCollections: List<PetCollection>) {
+    val filters = remember { PetRepo.getFilters() }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text(stringResource(id = R.string.appbar_title))
+            })
+        }) {
+
+        LazyColumn {
+            item {
+                Spacer(Modifier.statusBarsHeight(additional = 4.dp))
+                FilterBar(filters)
+            }
+            items(petCollections) { collection ->
+                PetCollection(collection, selectPet)
+            }
+        }
+        Spacer(Modifier.height(8.dp))
+    }
+}
+
+@Composable
+private fun PetCollection(
+    collection: PetCollection,
+    selectPet: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier) {
+        Text(
+            text = collection.name,
+            style = MaterialTheme.typography.h4,
+            color = MaterialTheme.colors.primary,
+            modifier = Modifier
+                .heightIn(min = 56.dp)
+                .padding(horizontal = 24.dp, vertical = 4.dp)
+                .wrapContentHeight()
+        )
+        VerticalGrid(Modifier.padding(horizontal = 16.dp)) {
+            collection.pets.forEach { pet ->
+                PetItem(pet, Modifier.padding(16.dp), openDetails = { selectPet(pet.petId) })
+
+            }
+        }
+        Spacer(Modifier.height(4.dp))
+    }
+}
+
+
+@Composable
+private
+fun PetItem(pet: Pet, modifier: Modifier, openDetails: () -> Unit = {}) {
     Card(
-        shape = MaterialTheme.shapes.small,
-        modifier = Modifier
+        elevation = 4.dp,
+        modifier = modifier
             .padding(
-                bottom = 6.dp,
-                top = 6.dp,
+                bottom = 4.dp,
+                top = 4.dp,
             )
             .fillMaxWidth()
-            .clickable(onClick = openDetails),
-        elevation = 8.dp,
-    ){
-        Surface(shape = RoundedCornerShape(8.dp), elevation = 8.dp, modifier = modifier) {
-            val image = painterResource(id = pet.imageResource)
-            Column(modifier = Modifier.fillMaxWidth()) {
-                Image(
-                    image,"${pet.name} Pet Image",
-                    Modifier
-                        .aspectRatio(1.77f, true)
-                        , Alignment.Center,ContentScale.Fit, )
+            .clickable(onClick = openDetails)
+    ) {
+        Column(
+            modifier = Modifier
+                .clickable(onClick = openDetails)
+                .fillMaxSize()
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(140.dp)
+                    .fillMaxWidth()
+                    .background(
+                        color = getBackgroundColorForElevation(Color.White, 4.dp),
+                    )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .fillMaxWidth()
+                        .background(
+                            color = getBackgroundColorForElevation(Color.LightGray, 4.dp),
+                        )
 
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text("Hi! I am "+pet.name, style = MaterialTheme.typography.h4, modifier = Modifier.padding(bottom = 4.dp))
-                    Text("My breed: "+pet.breed,style = MaterialTheme.typography.body1, modifier = Modifier.padding(bottom = 4.dp))
-                }
+                )
+                PetImage(
+                    imageResource = pet.imageResource,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(130.dp)
+                        .align(Alignment.BottomCenter)
+                )
             }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                pet.name,
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                pet.breed,
+                style = MaterialTheme.typography.caption,
+                modifier =  Modifier.align(Alignment.CenterHorizontally)
+            )
         }
     }
 }
