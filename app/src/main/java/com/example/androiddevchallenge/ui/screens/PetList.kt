79ac387653a645616823bf9cd2e4f5a7eb39ccdb
VarshaSameer <km.varsha@gmail.com>
2021-03-01 09:08:18 +0530

    update, fix: ui to incorporate added filters and pet details, code-formatting, spotless issues

--- app/src/main/java/com/example/androiddevchallenge/ui/screens/PetList.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screens/PetList.kt
@@ -1,16 +1,32 @@
 package com.example.androiddevchallenge.ui.screens
 
-
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.toggleable
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.FilterList
 import androidx.compose.runtime.Composable
@@ -19,6 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -31,7 +48,6 @@ import com.example.androiddevchallenge.ui.components.PetImage
 import com.example.androiddevchallenge.ui.components.VerticalGrid
 import com.example.androiddevchallenge.ui.components.getBackgroundColorForElevation
 import dev.chrisbanes.accompanist.insets.statusBarsHeight
-
 
 @Composable
 fun FilterBar(filters: List<Filter>) {
@@ -49,13 +65,14 @@ fun FilterBar(filters: List<Filter>) {
                     contentDescription = stringResource(R.string.label_filters),
                     modifier = Modifier.background(
                         color = Color.White,
-                        shape = CircleShape
+                        shape = RectangleShape
                     )
 
                 )
             }
         }
         items(filters) { filter ->
+
             FilterChip(filter)
         }
     }
@@ -76,8 +93,7 @@ fun FilterChip(
     )
     Surface(
         modifier = modifier
-            .height(30.dp)
-            ,
+            .height(30.dp),
         color = backgroundColor,
         contentColor = textColor,
         shape = shape,
@@ -108,10 +124,13 @@ fun PetList(selectPet: (Int) -> Unit, petCollections: List<PetCollection>) {
 
     Scaffold(
         topBar = {
-            TopAppBar(title = {
-                Text(stringResource(id = R.string.appbar_title))
-            })
-        }) {
+            TopAppBar(
+                title = {
+                    Text(stringResource(id = R.string.appbar_title))
+                }
+            )
+        }
+    ) {
 
         LazyColumn {
             item {
@@ -145,13 +164,11 @@ private fun PetCollection(
         VerticalGrid(Modifier.padding(horizontal = 16.dp)) {
             collection.pets.forEach { pet ->
                 PetItem(pet, Modifier.padding(16.dp), openDetails = { selectPet(pet.petId) })
-
             }
         }
         Spacer(Modifier.height(4.dp))
     }
 }
-
 
 @Composable
 private
@@ -205,7 +222,7 @@ fun PetItem(pet: Pet, modifier: Modifier, openDetails: () -> Unit = {}) {
             Text(
                 pet.breed,
                 style = MaterialTheme.typography.caption,
-                modifier =  Modifier.align(Alignment.CenterHorizontally)
+                modifier = Modifier.align(Alignment.CenterHorizontally)
             )
         }
     }
